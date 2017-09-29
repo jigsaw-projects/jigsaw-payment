c422fe92152880c8652df0546d7d467a302f4029
shamphone <shamphone@gmail.com>
2017-09-29 17:28:40 +0800

    test jenkins

--- jigsaw-mode-fastpay/src/main/java/org/jigsaw/payment/fastpay/Application.java
+++ jigsaw-mode-fastpay/src/main/java/org/jigsaw/payment/fastpay/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
- * 快捷支付应用入口，使用spring boot 来启动 。
+ * 快捷支付应用入口，使用spring boot 来启动。注意启动的时候需要指定profile。 
  * @author shamphone@gmail.com
  * @version 1.0.0
  * @date 2017年9月1日
